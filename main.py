a17e07a4ca5c41e65dc9a0279a09f6a3716a10dc
xenixsolus <hemantkumarsonare@gmail.com>
2025-12-07 18:48:48 +0530

    adding file upload  logic

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import Session
 from database import init_db, get_session
-from auth import get_current_user
-import crud, schemas
+from auth import get_current_user, verify_google_token
+from fastapi.responses import JSONResponse
+import crud, schemas, os
 
 app = FastAPI(title="E2EE Drive Backend")
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 # CORS
 app.add_middleware(
@@ -36,9 +40,16 @@ def list_folders(parent_id: int | None = None,
     path = []
     if parent_id is not None:
         path = crud.get_parent_chain(session, parent_id)
+        
+    # Return files inside this folder
+    files = []
+    folder_path = "uploads"
+    for fname in os.listdir(folder_path):
+        files.append({"name": fname})
 
     return {
         "folders": folders,
+        "files": files,
         "path": path
     }
 
@@ -46,3 +57,18 @@ def list_folders(parent_id: int | None = None,
 @app.get("/folders/all", response_model=list[schemas.FolderRead])
 def get_all_folders(user_id: str = Depends(get_current_user), session: Session = Depends(get_session)):
     return crud.get_all_folders(session, user_id)
+
+
+@app.post("/upload")
+async def upload_file(
+    file: UploadFile = File(...),
+    folder_id: str = Form(None),
+    user = Depends(verify_google_token)   # <-- this verifies Google token
+):
+    file_location = os.path.join(UPLOAD_DIR, file.filename)
+
+    # Save file
+    with open(file_location, "wb") as f:
+        f.write(await file.read())
+
+    return {"message": "OK", "file": file.filename, "folder_id": folder_id}
